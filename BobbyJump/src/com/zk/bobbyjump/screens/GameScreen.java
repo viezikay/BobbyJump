6673502c540fd0a83cb2876d2bbf67493737f88d
vieZikay <ndviettien.zk@gmail.com>
2014-04-21 20:36:36 +0700

    lol

--- BobbyJump/src/com/zk/bobbyjump/screens/GameScreen.java
+++ BobbyJump/src/com/zk/bobbyjump/screens/GameScreen.java
@@ -32,7 +32,6 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -54,25 +53,21 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 		
 	}
 
